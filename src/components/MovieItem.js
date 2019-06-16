549b3f8041a946328b589625c4fcd8ea90471016
andrew leong <andrew.leong@jewelpaymentech.com>
2019-06-16 11:51:31 +0800

    Align image width correctly

--- src/components/MovieItem.js
+++ src/components/MovieItem.js
@@ -4,10 +4,10 @@ const API_IMAGE = process.env.REACT_APP_API_IMAGE;
 
 const MovieItem = ({ movie, onMovieClick }) => {
     return (
-        <div className="movie-item-container" onClick={onMovieClick} src={`${API_IMAGE}w300/${movie.poster_path}`} alt="">
+        <div className="col-sm-12 movie-item-container" onClick={onMovieClick} src={`${API_IMAGE}w300/${movie.poster_path}`} alt="">
             <img src={`${API_IMAGE}w300/${movie.poster_path}`} alt="" />
-            <h3>{movie.title}</h3>
-            <h4>{movie.release_date}</h4>
+            <h5>{movie.title}</h5>
+            <h6>{movie.release_date}</h6>
         </div>
     )
 };
