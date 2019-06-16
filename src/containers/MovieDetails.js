1ed6e59eabd85e891baf9289cacadaa19453cc10
andrew leong <andrew.leong@jewelpaymentech.com>
2019-06-16 10:46:09 +0800

    Add movie details container and component

---
+++ src/containers/MovieDetails.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class MovieDetails extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            movie: {}
+        }
+    }
+
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+
+}
+
+export default MovieDetails;
