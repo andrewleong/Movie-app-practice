e6f7faeb34f9deec6f68d98d4ef81a6699c93e5b
andrew leong <andrew.leong@jewelpaymentech.com>
2019-06-16 15:10:59 +0800

    Add more movie details tests

--- src/__tests__/MovieDetails.test.js
+++ src/__tests__/MovieDetails.test.js
@@ -1,9 +1,29 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import MovieDetails from '../containers/MovieDetails';
+import MovieDetailsComponent from '../components/MovieDetails';
 
 const API_SITE = process.env.REACT_APP_API_SITE;
 const API_KEY = process.env.REACT_APP_API_KEY;
+
+
+describe('(Component) MovieDetails', () => {
+    it('should render a modal of movie details correctly', () => {
+        const movie = {
+            title: 'movie 1',
+            id: 122,
+            release_date: '12-12-2019'
+        }
+        const wrapper = mount(
+            <MovieDetailsComponent
+                movie={movie}
+            />
+        );
+
+        expect(wrapper.props()).toEqual({movie});
+        expect(wrapper.props()).toMatchSnapshot();
+    })
+});
 
 describe('(Container) MovieDetails', () => {
     it('should fetch movie details when rendered', () => {
